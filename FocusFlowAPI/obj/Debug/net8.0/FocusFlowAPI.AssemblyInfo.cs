0e69e09cf2ac6815b2f59d4e9079c9a38e788c73
Briseth Marin <sofiamarin69@gmail.com>
2026-04-14 12:32:03 -0500

    Final Sprint 1 commit

--- FocusFlowAPI/obj/Debug/net8.0/FocusFlowAPI.AssemblyInfo.cs
+++ FocusFlowAPI/obj/Debug/net8.0/FocusFlowAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FocusFlowAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4312e06cd7ab76b0c35e583db9a5e0ddb7a2f99c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974066c4753b51d3a70aaebdf429f8049eef3c3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FocusFlowAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FocusFlowAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
